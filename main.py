58450e432508044ddcf6432a9784d1a5ef2f46c2
Nabneet-cbc <nabneetsingh@creativebuffer.com>
2025-07-15 18:33:11 +0530

    changes

--- main.py
+++ main.py
@@ -113,5 +113,7 @@ def transcribe_from_youtube(req: YouTubeURLRequest):
         raise HTTPException(status_code=500, detail="Unexpected error: " + str(e))
 
 if __name__ == "__main__":
-    
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Use PORT from Render
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
